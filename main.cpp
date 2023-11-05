43231ca21a712a60b60a3670e9edcadb5c5ec7ae
Igor Graziano <igorrsgraziano@gmail.com>
2023-11-05 19:00:03 -0300

    created config file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
+#include <mysql/mysql.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
+struct mysql_config
+{
+    string host;
+    string user;
+    string password;
+    string database;
+    string port;
+} config;
+
+vector<string> split(const string &s, char delim)
+{
+    vector<string> result;
+    stringstream ss(s);
+    string item;
+
+    while (getline(ss, item, delim))
+    {
+        result.push_back(item);
+    }
+
+    return result;
+}
+
+void read_config_file()
+{
+    ifstream file("conn.cfg");
+    std::map<string, string> map;
+    if (file.is_open())
+    {
+        while (file.good())
+        {
+            string line;
+            getline(file, line);
+            vector<string> tokens = split(line, '=');
+            if (!tokens.size() == 2)
+                continue;
+            map[tokens[0]] = tokens[1];
+        }
+    };
+    config.database = map["DATABASE"];
+    config.host = map["HOST"];
+    config.port = map["PORT"];
+    config.user = map["USER"];
+    config.password = map["PASSWORD"];
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string test = "test";
+    string *ref = &test;
+    read_config_file();
+    cout << config.database;
+    MYSQL *conn;
+
     return 0;
 }
